26c393b482f531791e029ba624a1a46006d83285
Jinny Yang <jinnyyang@Jinnys-MacBook-Pro-7.local>
2019-06-13 01:09:38 -0400

    Fix typo in after request function

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 import os
 import logging
 from flask import Flask, request, jsonify
-
+import traceback
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello():
-    try:
-        return "OK"
-    except Exception as e:
-        app.logger.error(e)
-        return e
+    return "OK"
 
 
 @app.after_request
 def after_request(response):
-    try:
-        if response.status_code != 500:
-            app.logger.error('%s %s %s %s %s', request.remote_addr, request.method,
-            request.scheme, request.full_path, request.status)
+    if response.status_code != 500:
+        app.logger.error('%s %s %s %s %s', request.remote_addr, request.method,
+        request.scheme, request.full_path, response.status)
 
-            return response
-    except Exception as e:
-        return e
+    return response
 
+
+@app.errorhandler(Exception)
+def exceptions(e):
+    tb = traceback.format_exc()
+    app.logger.error('%s %s %s %s 5xx INTERNAL SERVER ERROR\n%s',
+    request.remote_addr, request.method, request.scheme, request.full_path, tb)
+    return 'bad request!'
 
 if __name__ == '__main__':
     app.run()
