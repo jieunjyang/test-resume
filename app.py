7307ae31844a35c4ed7586edfa848fb50ac3986e
Jinny Yang <jinnyyang@Jinnys-MacBook-Pro-7.local>
2019-06-13 00:22:09 -0400

    Change return message to match expected ping

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     try:
-        return "Hello World."
+        return "OK"
     except Exception as e:
         app.logger.error(e)
         return e
