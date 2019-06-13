b144bf9824d776be310203920ab631fd17d8e0d2
Jinny Yang <jinnyyang@Jinnys-MacBook-Pro-7.local>
2019-06-13 00:19:34 -0400

    Create basic flask app

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        return "Hello World."
+    except Exception as e:
+        app.logger.error(e)
+        return e
+
+@app.route("/api/v1/test", methods=['GET'])
+def get_test():
+    app.logger.info('Test log2')
+    print('print statement')
+    app.logger.error('An error occured')
+    app.logger.warning('A warning occured')
+    return "Test."
+
+if __name__ == '__main__':
+    app.run()
